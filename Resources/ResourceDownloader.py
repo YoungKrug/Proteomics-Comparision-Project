2fbac25083c9e674ec32d017b36114d8f439d8ea
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2023-03-09 18:54:29 -0500

    Created the basic Resource Development Pipeline

---
+++ Resources/ResourceDownloader.py
@@ -0,0 +1,7 @@
+class ResourceDownloader: # Will be the downloader method for the pride datasets
+    def __init__(self):
+        pass
+    def DownloadResources(self):
+        pass
+    def ImportResources(self):
+        pass
