750c7b172c9673968ef97813312c5661045ffb2b
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2023-03-15 16:38:03 -0400

    Resources (#1)
    * Created the basic Resource Development Pipeline
    
    * Making a resources downloader
    
    * Basic Pride resource downloading framework
    
    To create a new database
    - Create it in the database folder
    - Make it a interface of the ResourceDownloader
    - Initiate it as a databasetype in resource downloader and call the functions
    This will ensure readability and functionality while giving us the flexibility of using other databases.
    
    * Modifications to downloading
    
    * Commenting out code

---
+++ Resources/ResourceDownloader.py
@@ -0,0 +1,7 @@
+class ResourceDownloader: # Will be the downloader method for the pride datasets
+    def __init__(self, database):
+        self.database = database
+    def DownloadResources(self):
+        self.database.DownloadResources()
+    def ImportResources(self):
+        self.database.ImportResources()
