f32f7c3b5a48d0b536e7ece80d6743b29c4f3c97
everinghameric <97627862+everinghameric@users.noreply.github.com>
2023-03-15 18:15:09 -0400

    Update AccessPride.py

--- AccessPride.py
+++ AccessPride.py
@@ -1,9 +1,11 @@
 import ppx
 
+# Enter you project ID
 proj = ppx.find_project("PXD000001")
 remote_files = proj.remote_files()
 print(remote_files)
 
+# Download a target remote file to your local drive
 downloaded = proj.download("F063721.dat-mztab.txt")
 print(downloaded)
 
