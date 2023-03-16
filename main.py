351f6391474c69710c8c52b5063dec2902111168
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2023-03-16 08:48:48 -0400

    Updating environment yml and reorganizing project.

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ from RawFIleConverter.RawConverter import  RawConverter
 # rawFile = "PrideResources/3df36a611c1086ec76a928f381f856b56bc79d0709b7a866d9d51e9db789a719-SAL2.raw"
 # data = RawQuant(RawFile=rawFile)
 # print(data.data)
-rawfile = RawConverter("PrideResources/89be958c243a1ba43d34a0b4d5bec4dbff8f416686fb6e8a58d4a2f8f6ec7ec3-Serum_3.raw")
-rawfile.ConvertToTSV("PrideResources", filename="Serum_3")
+# rawfile = RawConverter("PrideResources/RawFiles/89be958c243a1ba43d34a0b4d5bec4dbff8f416686fb6e8a58d4a2f8f6ec7ec3-Serum_3.raw")
+# rawfile.ConvertToTSV("PrideResources/ConvertedData", filename="Serum_3.1")
