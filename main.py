e575395a79b03e3414267cc7f2ac15fc94c91b5c
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2023-03-16 08:40:06 -0400

    File Converter has been created, the readme file will be update with information on how to use the tool (#2)

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-from Resources import ResourceDownloader
+from RawFIleConverter.RawConverter import  RawConverter
+#from RawQuant import RawQuant
+
+
+#
+# rawFile = "PrideResources/3df36a611c1086ec76a928f381f856b56bc79d0709b7a866d9d51e9db789a719-SAL2.raw"
+# data = RawQuant(RawFile=rawFile)
+# print(data.data)
+rawfile = RawConverter("PrideResources/89be958c243a1ba43d34a0b4d5bec4dbff8f416686fb6e8a58d4a2f8f6ec7ec3-Serum_3.raw")
+rawfile.ConvertToTSV("PrideResources", filename="Serum_3")
